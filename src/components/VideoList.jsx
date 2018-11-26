8cbc52536c4e45667b1db5201285f462efef5f2a
mnutt346 <mnutt346@gmail.com>
2018-11-26 16:10:10 -0600

    Render VideoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,26 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
 var VideoList = () => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {exampleVideoData.map(video => {
+
+      let title = video.snippet.title;
+      let description = video.snippet.description;
+      let image = video.snippet.thumbnails.default.url;
+
+      return (
+        <VideoListEntry title={title} description={description} image={image} />
+      );
+    })}
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
-};
+// VideoList.propTypes = {
+//   videos: React.PropTypes.array.isRequired
+// };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
