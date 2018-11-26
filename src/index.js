1df2d1d1d0942cc055ebd68d4cb208270b74875e
mnutt346 <mnutt346@gmail.com>
2018-11-26 15:42:19 -0600

    Bypass CORS and render app.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+//import React from 'file://Volumes/THAWSPACE/hratx38-recast.ly/node_modules/react/react.js';
 
+ReactDOM.render(<App />, document.getElementById('app'));
