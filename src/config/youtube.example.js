92d57fc5ef749b5c9a3fd44f6a6d4e4d0f832341
mnutt346 <mnutt346@gmail.com>
2018-11-27 12:38:24 -0600

    searchYouTube ajax query can return results.

--- src/config/youtube.example.js
+++ src/config/youtube.example.js
@@ -1,4 +1,4 @@
 // Put your YouTube API keys here!
-var YOUTUBE_API_KEY = 'YOUR_API_KEY_HERE';
+var YOUTUBE_API_KEY = 'AIzaSyCGR3vI9TiJoRJ5NV3gqT_vke0hGNy85so';
 
 export default YOUTUBE_API_KEY;
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  $.ajax(
+    {
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      method: 'GET',
+      dataType: 'json',
+      success: (obj) => console.log(obj),
+      error: () => console.log('ERROR'),
+      data: JSON.stringify({
+        key: YOUTUBE_API_KEY,
+        q: 'Chubawumba',
+        part: 'snippet',
+        type: 'video',
+        videoEmbeddable: 'true'
+      }).replace(/"/g, '').replace(/:/g, '=').replace(/,/g, '&').slice(1, -1)
+    });
 };
 
 export default searchYouTube;
